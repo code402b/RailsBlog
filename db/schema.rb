95af324e163da516741efc9816d6e0d83dc73e5a
Anthony McGovern <mcgovern.anthony@gmail.com>
2015-07-07 15:19:00 -0400

    initialize comment resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707172551) do
+ActiveRecord::Schema.define(version: 20150707191703) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
