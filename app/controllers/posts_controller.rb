7c28aa36fb833b04faf682d54627f9ebb8b8eeeb
Andrew <Asaganda@gmail.com>
2015-07-07 16:49:30 -0400

    new post and create methods for post added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    @post.user = current_user
+
+    @post.create
+    flash[:notice] = "Post Created" 
+  end
+
 end
