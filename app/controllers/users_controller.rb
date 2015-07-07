14641e3018d66c35423b254335a89f6c6f199b60
Anthony McGovern <mcgovern.anthony@gmail.com>
2015-07-07 17:55:48 -0400

    add password fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,22 @@
 class UsersController < ApplicationController
 
+  @users = User.all
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    password          = params[:user][:password]
+    password_confirm  = :password_confirmation
+
+    if password == password_confirm
+      @user = User.create(params[:user])
+      flash[:notice] = "#{@user.username} created"
+      redirect_to users_path
+    else
+      flash[:alert] = "passwords don't match"
+    end
+  end
+
 end
