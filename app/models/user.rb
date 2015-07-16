f19f0b9e3519d4e9f8ef5e37f2cca9105cc40f72
Andrew <Asaganda@gmail.com>
2015-07-16 15:10:31 -0400

    Add username validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	validates :username, uniqueness: true
 	# user must input a unique username when creating a new user account
+	
 	has_many :posts
 	has_many :comments
 end
